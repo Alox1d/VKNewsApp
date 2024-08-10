4d9308c2fd1b000f8ffe6c9405541443500701de
Alox1d <alox1dab@gmail.com>
2024-08-10 19:41:15 +0300

    9.2.2 error handling for comments (simple way without sealed class for Error + Result)

--- app/src/main/java/ru/alox1d/vknewsapp/data/repository/NewsFeedRepository.kt
+++ app/src/main/java/ru/alox1d/vknewsapp/data/repository/NewsFeedRepository.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -88,13 +89,16 @@ class NewsFeedRepository(application: Application) {
         nextDataNeededEvents.emit(Unit)
     }
 
-    suspend fun getComments(feedPost: FeedPost): List<PostComment> {
+    fun getComments(feedPost: FeedPost): Flow<List<PostComment>> = flow {
         val comments = apiService.getComments(
             accessToken = getAccessToken(),
             ownerId = feedPost.communityId,
             postId = feedPost.id
         )
-        return mapper.mapResponseToComments(comments)
+        emit(mapper.mapResponseToComments(comments))
+    }.retry {
+        delay(RETRY_TIMEOUT_MILLIS)
+        true
     }
 
     suspend fun changeLikeStatus(feedPost: FeedPost) {
--- app/src/main/java/ru/alox1d/vknewsapp/presentation/comments/CommentsViewModel.kt
+++ app/src/main/java/ru/alox1d/vknewsapp/presentation/comments/CommentsViewModel.kt
@@ -1,11 +1,8 @@
 package ru.alox1d.vknewsapp.presentation.comments
 
 import android.app.Application
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.map
 import ru.alox1d.vknewsapp.data.repository.NewsFeedRepository
 import ru.alox1d.vknewsapp.domain.FeedPost
 
@@ -16,20 +13,11 @@ class CommentsViewModel(
 
     private val repository = NewsFeedRepository(application)
 
-    private val _screenState = MutableLiveData<CommentsScreenState>(CommentsScreenState.Initial)
-    val screenState: LiveData<CommentsScreenState> = _screenState
-
-    init {
-        loadComments(feedPost)
-    }
-
-    private fun loadComments(feedPost: FeedPost) {
-        viewModelScope.launch {
-            val comments = repository.getComments(feedPost)
-            _screenState.value = CommentsScreenState.Comments(
+    val screenState = repository.getComments(feedPost)
+        .map {
+            CommentsScreenState.Comments(
                 feedPost = feedPost,
-                comments = comments
+                comments = it
             )
         }
-    }
 }
